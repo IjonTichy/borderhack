d2f16f981d82c34fc86bc833ef7b62671ceffef7
Ijon Tichy <gztichy@lavabit.com>
2014-02-20 21:50:56 -0600

    Super necessary title change

--- src/core/GameWindow.java
+++ src/core/GameWindow.java
@@ -33,7 +33,7 @@ public class GameWindow
         VideoMode    gameres            = new VideoMode(1024, 768);
         BlockingQueue<Event> evQueue    = new LinkedBlockingQueue<Event>();
         
-        gamewin.create(gameres, "Borderhack: 4% of your recommended diet", RenderWindow.CLOSE | RenderWindow.TITLEBAR);
+        gamewin.create(gameres, "Borderhack: 8% of your recommended diet", RenderWindow.CLOSE | RenderWindow.TITLEBAR);
         gamewin.setFramerateLimit(Constants.FRAME_LIMIT);
 
 
